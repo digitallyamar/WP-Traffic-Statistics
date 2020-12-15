cb55685d3994a07f40e897e788bef6210c5a9c4f
digitallyamar <digitallyamar@gmail.com>
2020-12-15 16:02:50 +0530

    Added basic files to kickstart WP Plugin detection in WP.

---
+++ WP-Traffic-Statistics.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: WP Traffic Statistics
+ * Plugin URI: https://muddoo.com
+ * Description: This plugin will track and report data related to traffic to your Wordpress website.
+ * Version: 1
+ * Author: Digitally Amar
+ * Author URI: https://twitter.com/digitallyamar
+ */
+
+ # Exit if accessed directly
+if (!defined('ABSPATH')) exit;
